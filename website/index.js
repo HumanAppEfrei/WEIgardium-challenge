3291ada4f0a56e2efebce056760632ff3f23125c
AmiralBl3ndic <camille.briand@efrei.net>
2018-07-19 14:35:28 +0200

    Change project organization

---
+++ website/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+
+
+const listeningPort = 1664;
+
+
+app.use("/downloads", express.static('public'));
+
+
+
+app.get('/', (req, res) => {
+  res.status(200).send("Hello world!");
+});
+
+
+app.post('/exercise1', (req, res) => {
+  console.log("Got a POST request on '/exercise1'");
+
+  // TODO: do some stuff here
+
+  res.status(200).json({
+    error: false,
+    message: "Exercice réussi !"
+  });
+});
+
+
+app.listen(listeningPort, () => console.log(`Listening on port ${listeningPort}`));
