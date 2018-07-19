14bcd8ebcba11cb6779f2ca83991585dd8d9062d
AmiralBl3ndic <camille.briand@efrei.net>
2018-07-19 14:12:15 +0200

    Start a basic express app

---
+++ back/index.js
@@ -0,0 +1,24 @@
+const app = require("express")();
+
+
+const listeningPort = 1664;
+
+
+app.get('/', (req, res) => {
+    res.status(200).send("Hello world!");
+});
+
+
+app.post('/exercise1', (req, res) => {
+   console.log("Got a POST request on '/exercise1'");
+
+   // TODO: do some stuff here
+
+   res.status(200).json({
+      error: false,
+      message: "Exercice réussi !"
+   });
+});
+
+
+app.listen(listeningPort, () => console.log(`Listening on port ${listeningPort}`));
