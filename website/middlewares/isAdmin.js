5afb59a689fff4b884fba0f3c936342b8f7c2fcd
AmiralBl3ndic <camille.briand@efrei.net>
2018-07-23 23:57:57 +0200

    Better admin security

--- website/middlewares/isAdmin.js
+++ website/middlewares/isAdmin.js
@@ -1,17 +1,37 @@
 const admins = require("./../config/admin");
 const hash = require("hash-sum");
+let db = require("./../config/db");
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
+
   if (typeof(req.body.firstName) !== "string")
     return res.status(400).redirect("/");
 
-  const requestedAdmin = admins[req.body.firstName];
+  db = db.connection ? db.connection : await db.connect();
 
-  if (requestedAdmin !== null && requestedAdmin !== undefined) {
-    if (typeof(req.body.firstName) === "string" && hash(req.body.password) === requestedAdmin.password) {
-      console.log("Admin connected");
-      next();
-    }
+  // Requesting possible administrators from database
+  const [rows, fields] = await db.execute("SELECT * FROM `Admin` WHERE `first_name` = ? AND `last_name` = ?", [req.body.firstName, req.body.lastName]);
+
+  let possibilities = [];
+  rows.forEach((val, index, array) => {
+    possibilities.push({
+      firstName: rows[index].first_name,
+      lastName: rows[index].last_name,
+      password: rows[index].password
+    });
+  });
+
+
+  let adminOk = null;
+  possibilities.forEach((admin, index, array) => {
+    if (admin.password === hash(req.body.password))
+      adminOk = admin;
+  });
+
+
+  if (adminOk != null) {
+    console.log("Admin connected!");
+    next();
   }
   else {
     return res.status(403).redirect("/");
